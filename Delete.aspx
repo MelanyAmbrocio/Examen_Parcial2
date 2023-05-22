077286a570eaf71152af985c6ba2890d8e122f35
MelanyAmbrocio@umesoamericana.onmicrosoft.com <MelanyAmbrocio@umesoamericana.onmicrosoft.com>
2023-05-22 09:41:27 -0600

    Eliminar animal ingresado

---
+++ Delete.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Delete.aspx.cs" Inherits="Examen_Parcial2.Delete" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Eliminar Animal</h1>
+    <p>
+        Buscar:
+        <asp:TextBox ID="txtBuscar" runat="server"></asp:TextBox>
+&nbsp;
+        <asp:Button ID="btnBuscar" runat="server" Text="Buscar" />
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblNombreAnimal" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnDelete" runat="server" Enabled="False" OnClick="btnDelete_Click" Text="Eliminar" />
+    </p>
+</asp:Content>
---
+++ Delete.aspx.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Examen_Parcial2
+{
+    public partial class Delete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Grabar(List<Animal> animales)
+        {
+            string json = JsonConvert.SerializeObject(animales);
+            string archive = Server.MapPath("Datos.json");
+            System.IO.File.WriteAllText(archive, json);
+        }
+
+        private List<Animal> Leer()
+        {
+            List<Animal> lista = new List<Animal>();
+            string archivo = Server.MapPath("Datos.json");
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            lista = JsonConvert.DeserializeObject<List<Animal>>(json);
+
+            return lista;
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            List<Animal> animales = Leer();
+            Animal animal = animales.Find(u => u.Nombre == txtBuscar.Text);
+            if (animal != null)
+            {
+                Response.Write("<script>alert { 'Animal encontrado' } </script");
+                lblNombreAnimal.Text = animal.Nombre;
+                btnDelete.Enabled = true;
+            }
+            else
+            {
+                Response.Write("<script>alert { 'Animal no encontrado' } </script");
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+
+           
+
+            List<Animal> animales = Leer();
+           
+            Animal animal = animales.Find(u => u.Nombre == lblNombreAnimal.Text);
+            lblNombreAnimal.Text = animal.Nombre;
+
+            animales.Remove(animal);
+            Response.Write("<script>alert('Animal eliminado!')</script>");
+
+            Grabar(animales);
+        }
+    }
+}
